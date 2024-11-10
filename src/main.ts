30d1c6290497c78ed62783e8aef999a2e7da6b88
homeniuk <homeniuk@i.ua>
2024-11-10 22:02:35 +0200

    deploy VPS2

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors();
   /*app.enableCors({
             origin: true,
             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
